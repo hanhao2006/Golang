c6fc547afdf8570212c14b9965cffe03b45dbf34
2Echo <49248731+hanhao2006@users.noreply.github.com>
2019-12-27 18:13:14 -0500

    change

--- DisplayStudent/main.go
+++ DisplayStudent/main.go
@@ -1,57 +1,57 @@
-// package main
+package main
 
-// import "fmt"
+import "fmt"
 
-// type Student struct {
-// 	name  string
-// 	grade int
-// }
+type Student struct {
+	name  string
+	grade int
+}
 
-// func main() {
+func main() {
 
-// 	var nb int
+	var nb int
 
-// 	fmt.Println("COLLEGE LASALLE")
+	fmt.Println("COLLEGE LASALLE")
 
-// 	for ok := true; ok; ok = nb > 21 {
-// 		fmt.Print("Enter the number of students (max:20): ")
-// 		fmt.Scan(&nb)
-// 	}
-// 	stu := readAllStudent(nb)
-// 	displayAllStu(stu)
+	for ok := true; ok; ok = nb > 21 {
+		fmt.Print("Enter the number of students (max:20): ")
+		fmt.Scan(&nb)
+	}
+	stu := readAllStudent(nb)
+	displayAllStu(stu)
 
-// }
+}
 
-// func readOneStuName() string {
-// 	var name string
-// 	fmt.Print("name: ")
-// 	fmt.Scan(&name)
+func readOneStuName() string {
+	var name string
+	fmt.Print("name: ")
+	fmt.Scan(&name)
 
-// 	return name
-// }
+	return name
+}
 
-// func readOneStuGrade() int {
-// 	var grade int
-// 	for ok := true; ok; ok = grade < 0 || grade > 101 {
-// 		fmt.Print("Grade: ")
-// 		fmt.Scan(&grade)
-// 	}
-// 	return grade
-// }
+func readOneStuGrade() int {
+	var grade int
+	for ok := true; ok; ok = grade < 0 || grade > 101 {
+		fmt.Print("Grade: ")
+		fmt.Scan(&grade)
+	}
+	return grade
+}
 
-// func readAllStudent(nb int) []Student {
-// 	stu := make([]Student, nb)
-// 	for i := 0; i < nb; i++ {
-// 		fmt.Println("Student ", i+1)
-// 		stu[i].name = readOneStuName()
-// 		stu[i].grade = readOneStuGrade()
-// 	}
-// 	return stu
+func readAllStudent(nb int) []Student {
+	stu := make([]Student, nb)
+	for i := 0; i < nb; i++ {
+		fmt.Println("Student ", i+1)
+		stu[i].name = readOneStuName()
+		stu[i].grade = readOneStuGrade()
+	}
+	return stu
 
-// }
+}
 
-// func displayAllStu(stu []Student) {
-// 	for _, s := range stu {
-// 		fmt.Println(s.name, s.grade)
-// 	}
-// }
+func displayAllStu(stu []Student) {
+	for _, s := range stu {
+		fmt.Println(s.name, s.grade)
+	}
+}
